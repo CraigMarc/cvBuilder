0f6e9df42a05f0dd5f770e397052bea3fadb29e7
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-10-23 20:42:39 -0400

    css

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -14,81 +14,88 @@ function Resume(props) {
         workData,
     } = props;
 
-   
+
 
     let schoolName = data.schoolName
 
     const arrayDataItems = schoolData.map(data =>
         <div key={data.id}>
             <div className="schoolResume">
-            <div>
-            <p>{data.startDate} - {data.endDate}</p>
-            <p>{data.location}</p>
-            </div>
-            <div>
-           <p><strong>{data.schoolName}</strong></p>
-            <p>{data.degree} {data.fos}</p>
-            </div>
+                <div>
+                    <p>{data.startDate} - {data.endDate}</p>
+                    <p>{data.location}</p>
+                </div>
+                <div>
+                    <p><strong>{data.schoolName}</strong></p>
+                    <p>{data.degree} {data.fos}</p>
+                </div>
             </div>
         </div>
 
     )
 
     const arrayWorkItems = workData.map(data =>
-      <div key={data.id} className="workResume">
-            
+        <div key={data.id} className="workResume">
+
             <div>
-            <p>{data.wStartDate} - {data.wEndDate}</p>
-            <p>{data.wLocation}</p>
+                <p>{data.wStartDate} - {data.wEndDate}</p>
+                <p>{data.wLocation}</p>
             </div>
             <div>
-            <p><strong>{data.company}</strong></p>
-            <p>{data.title}</p>
-            <p>{data.description}</p>
+                <p><strong>{data.company}</strong></p>
+                <p>{data.title}</p>
+                <p>{data.description}</p>
             </div>
-            
+
         </div>
 
     )
 
-    
-    
+
+
 
     return (
-       <div className="resumeContainer">
-        <div className="resume">
-            <header>
+        <div className="resumeContainer">
+            <div className="resume">
+                <header>
 
-                <h1>{data.name}</h1>
-                <div className="container">
-                <h2><img className="emailImg" src={email}></img> {data.email}</h2>
-                
-                <h2><img className="phoneImg" src={phone}></img> {data.phone}</h2>
-                
-                <h2><img className="locImg" src={location}></img>{data.address}</h2>
+                    <h1>{data.name}</h1>
+                    <div className="container">
+                        <div className="basicCont">
+                            <img className="emailImg" src={email}></img>
+                            <h2> {data.email}</h2>
+                        </div>
+                        <div className="basicCont">
+                            <img className="phoneImg" src={phone}></img>
+                            <h2>{data.phone}</h2>
+                        </div>
+                        <div className="basicCont">
+                            <img className="locImg" src={location}></img>
+                            <h2>{data.address}</h2>
+                        </div>
+                    </div>
+
+                </header>
+                <div className="ed">
+                    <h3>Education</h3>
+                    <div>{arrayDataItems}</div>
+
                 </div>
 
-            </header>
-            <div className="ed">
-                <h3>Education</h3>
-                <div>{arrayDataItems}</div>
+                <div className="work">
+                    <h3>Work Experience</h3>
+                    <div>{arrayWorkItems}</div>
+
+                </div>
+
+
+
 
             </div>
-
-            <div className="work">
-                <h3>Work Experience</h3>
-                <div>{arrayWorkItems}</div>
-
-            </div>
-
-
-            
-            
-          </div>
         </div>
-   
 
-        
+
+
     )
 }
 
